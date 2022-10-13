a7bb26ca2d6f26ea0ce244881a45048e36cc660c
nguuyenthithuy <115654221+nguuyenthithuy@users.noreply.github.com>
2022-10-13 20:31:09 +0700

    Add files via upload

---
+++ buoi5/buoi5.js
@@ -0,0 +1,15 @@
+
+
+console.log('Hello Nguyễn Thị Thủy')
+let a = 5;
+let b = 6.9;
+let c = 6;
+let d = '5';
+console.log(a + b)
+console.log(a - d) // trừ nhân chia nó cố gắng đem chuỗi về số
+console.log(a === d, 'a value :', typeof a, 'd value :', typeof d)
+// so sánh chuỗi với số ra false và kiểu dữ liệu của a và d
+let e = +d; // let e= number(d)
+
+console.log(a === e, 'a value : ', typeof a, 'd value :', typeof e)
+// chuyển kiểu dữ liệu chuỗi thành số
