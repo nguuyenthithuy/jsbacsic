4298ae5a05bb7b2ac07c977d30f6417bd376a3c9
nguuyenthithuy <115654221+nguuyenthithuy@users.noreply.github.com>
2022-10-13 23:10:25 +0700

    Add files via upload

---
+++ buoi8/buoi8.js
@@ -0,0 +1,28 @@
+
+console.log('Hello Nguyễn Thị Thủy')
+
+let a = {
+    name: 'Nguyễn Thị Thủy',
+    age: 19,
+}; // key:value
+
+console.log('check data object :', a)
+console.log('my name is ', a.name)
+
+//arr=[item1,item2,item3]
+let b = ['MU', 'Manct', 'Chelsea']
+console.log('typof b:', typeof b, b)
+console.log('first item:', b[0], 'second item:', b[1])
+
+let family = [
+    {
+        name: 'Nguyễn Thị Thủy',
+        age: 19,
+    },
+    {
+        name: 'Nguyễn Thành Đạt',
+        age: 19,
+    },
+]
+
+console.log('my family: ', family)
