bff691aeae7fdc0621803e7ef932ee1a1a1a854c
nguuyenthithuy <115654221+nguuyenthithuy@users.noreply.github.com>
2022-10-13 18:05:02 +0700

    Add files via upload

---
+++ buoi4/buoi4.js
@@ -0,0 +1,8 @@
+let a = 'Tôi yêu bạn string-chuỗi'
+console.log(a);
+console.log(4 + 5 + 'a');
+//   = 9 + 'a'=9a
+console.log('a' + 4 + 5);
+// = a4 + 5=a45
+let text = ` i'm nguyễn thị thủy `
+console.log(text);
