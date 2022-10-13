087c5bd03d23d2c4a8bcdb646141933d02833b63
nguuyenthithuy <115654221+nguuyenthithuy@users.noreply.github.com>
2022-10-13 21:31:49 +0700

    Add files via upload

---
+++ buoi6/buoi6.js
@@ -0,0 +1,9 @@
+
+console.log('Hello Nguyễn Thị Thủy')
+let a = 7
+let b = 9
+let c = 38
+//url: /nguyenthithuy/38
+
+console.log(`Hoi nguyen thi thuuy : a=${a} b=${b} a+b=${a + b}`)
+console.log(`/nguyenthithuy/${c}`)
