ea32595f82bcc5f6d51c972343e805c9232aa4ad
nguuyenthithuy <115654221+nguuyenthithuy@users.noreply.github.com>
2022-10-14 17:13:20 +0700

    Add files via upload

---
+++ buoi10/buoi10.js
@@ -0,0 +1,12 @@
+
+console.log('Hello Nguyễn THỊ Thủy')
+
+let arr = ['MU', 'Liver', 'ManCity', 'Chelsea']
+
+let i = 0
+
+for (let i = 0; i < arr.length; i++) {
+    console.log('check i = ', arr[i])
+}
+
+console.log('check i = ', i)
