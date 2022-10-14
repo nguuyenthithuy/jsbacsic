f73f684baaa7b4be75212a8eb751077e37bb7a49
nguuyenthithuy <115654221+nguuyenthithuy@users.noreply.github.com>
2022-10-14 18:05:00 +0700

    Add files via upload

---
+++ buoi11/buoi11.js
@@ -0,0 +1,16 @@
+console.log('Hello Nguyễn THị Thủy')
+let arrTop4 = ['Man city', 'Liverpool', 'MU', 'Chelsea'];
+
+for (let i = 0; i < arrTop4.length; i++) {
+    console.log('Top: ', i + 1, arrTop4[i])
+}
+
+let i = 0
+
+let flag = false;
+
+while (!flag) {
+    console.log('check value i: ', i)
+    i++;
+    if (i === 10) flag = true;
+}
