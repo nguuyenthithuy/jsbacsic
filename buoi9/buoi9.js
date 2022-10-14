7c5bc0f749493d7a70239f44eba2e97eac470af5
nguuyenthithuy <115654221+nguuyenthithuy@users.noreply.github.com>
2022-10-14 16:06:24 +0700

    Add files via upload

---
+++ buoi9/buoi9.js
@@ -0,0 +1,20 @@
+
+
+console.log('Hello Nguyễn Thị Thủy')
+
+let a = 5
+let b = '5'
+
+console.log('compare a==b:', 'a = ', a, 'b = ', b, a == b)
+console.log('compare a===b:', 'a = ', a, 'b = ', b, a === b)
+// ===type,value: so sánh cả về kiểu dữ liệu
+// == value: chỉ so sánh về giá trị
+
+// empty, null, underfined
+// empty là rỗng khia báo kiểu let c='',c[],c{}
+// underfined là không khai báo biến  let c,let d;
+
+let c = null
+console.log('check value :', c)
+
+
