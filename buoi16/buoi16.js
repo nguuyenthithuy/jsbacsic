10d92013acb7c977119d6a1a273c3b59556618a1
nguuyenthithuy <115654221+nguuyenthithuy@users.noreply.github.com>
2022-10-15 22:50:46 +0700

    Add files via upload

---
+++ buoi16/buoi16.js
@@ -0,0 +1,14 @@
+
+console.log('Hello Nguyễn Thị Thủy')
+
+// function sum(x,y){
+//     return x+y
+// }
+
+// ArrOW Function
+
+let sum = (x, y) => {
+    return x + y;
+}
+
+console.log('check sum = ', sum(4, 5))
