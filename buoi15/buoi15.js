2a83e1617d1709171a575b1383073e0ef3288ca2
nguuyenthithuy <115654221+nguuyenthithuy@users.noreply.github.com>
2022-10-15 22:34:04 +0700

    Add files via upload

---
+++ buoi15/buoi15.js
@@ -0,0 +1,17 @@
+
+console.log('Hello Nguyễn Thị Thủy')
+
+function Tinhtong(x, y, z) {
+    console.log('Hello 1');
+    console.log('Hello 2');
+    console.log('Hello 3');
+
+    if (z === 5) return;
+
+    console.log('Hello 4')
+    console.log('Hello 5')
+
+    return x + y;
+}
+
+console.log('Sum = ', Tinhtong(3, 4, 5))
